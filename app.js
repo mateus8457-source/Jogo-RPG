764b28b67e696fc0c07a10e12b6d747d50edd08f
mateus8457-source <mateus.8457@colegioprogresso.g12.br>
2026-08-12 09:27:01 -0300

    Acabando o RPG

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
-    const guerreiro = { 
-    nome: "Thorin", 
-    vida: 60, 
-    ataque: 20, 
-    
-    causaDano: function(personagem){ 
-    personagem.recebeDano(this.ataque) 
-    },
-    
-    recebeDano: function(quantidade){ 
-    this.vida = this.vida - quantidade 
-    }   
-} 
+    class Personagem{
+      constructor(nome, vida, ataque){
+        this.nome = nome
+        this.vida = vida
+        this. ataque = ataque
+      }
 
-    const mago = { 
-    nome: "Gandalfe", 
-    vida: 60, 
-    ataque: 35, 
+      causaDano(personagem){
+        personagem.recebeDano(this.ataque)
     
-    recebeDano: function(quantidade){ 
-    this.vida = this.vida - quantidade 
- },
- 
- causaDano: function(personagem){ 
-    personagem.recebeDano(this.ataque) 
+      }
+
+      recebeDano(quantidade){
+        this.vida  =this.vida - quantidade
+
+
+        if(this.vida <= 0){
+            this.vida = "eliminado"
+        }
+      }
+
     }
-} 
 
-    const arqueiro = { 
-    nome: "Legolas", 
-    vida: 80, 
-    ataque: 25, 
     
-    causaDano: function(personagem){ 
-    personagem.recebeDano(this.ataque) 
-    },
-    
-    recebeDano: function(quantidade){ 
-    this.vida = this.vida - quantidade 
-    }   
 
-} 
+const druida = new Personagem("Kode",80 ,25 )
+const guerreiro = new Personagem("Thorin", 60,20)
+const mago = new Personagem("Gandalfe", 60, 35 )
+const arqueiro = new Personagem("Legolas", 80, 25)
 
-guerreiro.causaDano(mago) 
-console.log(mago.vida) 
+console.log(druida, guerreiro, mago, arqueiro)
 
