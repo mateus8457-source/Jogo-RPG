7cacee318dab8fcc1d63fe7f9bc35cdeda137fe9
mateus8457-source <mateus.8457@colegioprogresso.g12.br>
2026-08-26 09:12:26 -0300

    Começando matéria nova

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-    class Personagem{
-      constructor(nome, vida, ataque){
-        this.nome = nome
-        this.vida = vida
-        this. ataque = ataque
-      }
+class Personagem{
+    constructor(nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
+    }
 
-      causaDano(personagem){
+    causaDano(personagem){
         personagem.recebeDano(this.ataque)
-    
-      }
+    }
 
-      recebeDano(quantidade){
-        this.vida  =this.vida - quantidade
-
+    recebeDano(quantidade){
+        this.vida = this.vida - quantidade
 
         if(this.vida <= 0){
             this.vida = "eliminado"
         }
-      }
-
-    }
-
     
+   } 
+   
+    estaVivo(){
+       return this.vida > 0 
+      }  
 
-const druida = new Personagem("Kode",80 ,25 )
-const guerreiro = new Personagem("Thorin", 60,20)
-const mago = new Personagem("Gandalfe", 60, 35 )
+      mostrarStatus(){ 
+      console.log(this.nome + " | Vida: " + this.vida) 
+} 
+
+
+}
+
+const druida = new Personagem("kode", 80, 25)
+const guerreiro = new Personagem("Thorin", 60, 20)
+const mago = new Personagem("Gandalfe", 60, 35)
 const arqueiro = new Personagem("Legolas", 80, 25)
+const tanque = new Personagem("Reinhardt", 90, 15)
+const necromante = new Personagem("Mortis", 70, 20)
+const assassino = new Personagem("Zephyr", 50, 30)
 
-console.log(druida, guerreiro, mago, arqueiro)
+druida.causaDano(mago)
+druida.causaDano(mago)
 
+console.log(mago.mostrarStatus())
